5a66bacdf0c774ddddf59144824d7751eb3abab8
Zhangwx <1351225890@qq.com>
2018-11-04 20:29:01 +0800

    上拉加载

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: mikey.zhang 
  * @Date: 2018-11-04 14:59:59 
  * @Last Modified by: mikey.zhang
- * @Last Modified time: 2018-11-04 15:06:07
+ * @Last Modified time: 2018-11-04 20:27:03
  */
 
 var gulp = require('gulp');
@@ -10,6 +10,10 @@ var fs = require('fs');
 var path = require('path');
 var url = require('url');
 var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var babel = require('gulp-babel');
 var data = require('./src/data/data.json');
 
 //搭建服务器
@@ -28,7 +32,12 @@ gulp.task('server', function() {
                 var pathname = url.parse(req.url).pathname;
                 if (/^\/api/.test(req.url)) {
                     if (pathname === '/api/getData') {
-                        res.end(JSON.stringify({ code: 0, data: data.data }));
+                        var idx = url.parse(req.url, true).query.idx;
+                        var start = idx * 4;
+                        var end = start + 4;
+                        var datas = data.data.slice(start, end);
+                        res.end(JSON.stringify({ code: 0, data: datas }));
+                        res.end('666')
                     } else {
                         res.end('请求失败!');
                     }
@@ -39,3 +48,34 @@ gulp.task('server', function() {
             }
         }));
 })
+
+//编译scss
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'));
+})
+
+//监听sass
+gulp.task('watch', function() {
+    return gulp.watch('./src/scss/style.scss', gulp.series('sass'))
+})
+
+//es6转es5并压缩js
+gulp.task('minjs', function() {
+    return gulp.src('./src/js/index.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./src/js/minjs'))
+})
+
+//压缩css
+gulp.task('minify-css', function() {
+    return gulp.src('./src/css/style.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./src/css/mincss'))
+})
+
+gulp.task('last', gulp.series('sass', 'minjs', 'minify-css', 'server', 'watch'));
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
  * @Author: mikey.zhang 
  * @Date: 2018-11-04 14:38:25 
  * @Last Modified by: mikey.zhang
- * @Last Modified time: 2018-11-04 15:48:16
+ * @Last Modified time: 2018-11-04 20:25:27
  */
 
 //swiper轮播图
@@ -16,15 +16,16 @@ var mySwiper = new Swiper('.container', {
 var myBScroll = new BScroll('section', {
     probeType: 2,
     click: 2,
-    scrollbar: true
+    //scrollbar: true
 })
 
 //ajax请求数据
-ajax();
+var idx = 0;
+ajax(0);
 
-function ajax() {
+function ajax(idx) {
     $.ajax({
-        url: '/api/getData',
+        url: '/api/getData?idx=' + idx,
         success: function(res) {
             res = JSON.parse(res);
             if (res.code === 0) {
@@ -49,5 +50,64 @@ function render(data) {
             </dl>
         `;
     })
+    $('.content').append(str);
+    myBScroll.refresh();
+}
+
+//上拉加载
+myBScroll.on('scroll', function() {
+    if (this.y < this.maxScrollY - 100) {
+        $('.box').attr('up', '释放加载');
+    } else if (this.y < 0 && this.y > this.maxScrollY - 100) {
+        $('.box').attr('up', '上拉加载……');
+    } else if (this.y > 0 && this.y < 70) {
+        $('.box').attr('down', '下拉刷新……');
+    } else if (this.y > 70) {
+        $('.box').attr('down', '释放刷新');
+    }
+});
+myBScroll.on('scrollEnd', function() {
+    if ($('.box').attr('up') === '释放加载') {
+        up();
+        $('.box').attr('up', '上拉加载……');
+    } else if ($('.box').attr('down') === '释放刷新') {
+        down();
+        $('.box').attr('down', '下拉刷新……');
+    } else if (this.y > 70) {}
+});
+
+function up() {
+    idx++;
+    ajax(idx);
+}
+
+function down() {
+    idx = 0;
+    $.ajax({
+        url: '/api/getData?idx=' + idx,
+        success: function(res) {
+            res = JSON.parse(res);
+            if (res.code === 0) {
+                render2(res.data);
+            }
+        }
+    })
+}
+
+function render2(data) {
+    var str = '';
+    data.map(function(item) {
+        str += `
+            <dl>
+                <dd>
+                    <p>${item.tit}</p>
+                    <p>${item.con}</p>
+                    <p>${item.time}</p>
+                </dd>
+                <dt><img src="./images/${item.img}" alt=""></dt>
+            </dl>
+        `;
+    })
     $('.content').html(str);
+    myBScroll.refresh();
 }
